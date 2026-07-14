d5987fc9a707c6805c5b676241ff045d7ad3837b
apwalca <apwalca88@gmail.com>
2026-07-15 02:07:07 +0300

    Create logger.go

---
+++ internal/logger/logger.go
@@ -0,0 +1,35 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+)
+
+type Logger struct {
+	*slog.Logger
+}
+
+func New(level string, serviceName string) *Logger {
+	var logLevel slog.Level
+	switch level {
+	case "debug":
+		logLevel = slog.LevelDebug
+	case "info":
+		logLevel = slog.LevelInfo
+	case "warn":
+		logLevel = slog.LevelWarn
+	case "error":
+		logLevel = slog.LevelError
+	default:
+		logLevel = slog.LevelInfo
+	}
+
+	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: logLevel,
+	})
+
+	logger := slog.New(handler)
+	logger = logger.With("service", serviceName)
+
+	return &Logger{logger}
+}
